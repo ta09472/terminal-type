97b2c21882a13606bf56ca4276546b44f78336e3
beomsuKim <ta09472@naver.com>
2024-04-22 13:49:14 +0900

    패치노트 수정

--- src/components/PatchNode.tsx
+++ src/components/PatchNode.tsx
@@ -12,8 +12,11 @@ export default function PatchNote({ open, onCancel, isLocal }: Props) {
         <div className="text-lg flex-col text-neutral-800">
           <div className=" overflow-auto px-9 flex-col gap-4">
             <div className="mb-4">
-              <div className="pb-4 font-semibold">0.41 패치내역</div>
-              <div className="text-base flex flex-col gap-4">
+              <div className="pb-4 font-semibold flex justify-between items-center">
+                <div>0.41 패치내역</div>
+                <div className="text-base text-neutral-700">2024.4.22</div>
+              </div>
+              <div className="text-sm flex flex-col gap-4">
                 <div>
                   - 프로 모드에서도 이제 사용자 설정 텍스트 색상이 적용됩니다.
                   <br />- 주어진 문장보다 입력한 글자수가 클 경우 더이상 문자가
@@ -22,8 +25,11 @@ export default function PatchNote({ open, onCancel, isLocal }: Props) {
               </div>
             </div>
             <div className="mb-4">
-              <div className="pb-4">0.4 패치내역</div>
-              <div className="text-base flex flex-col gap-4">
+              <div className="pb-4 flex justify-between  items-center">
+                <div>0.4 패치내역</div>
+                <div className="text-base text-neutral-700">2024.4.16</div>
+              </div>
+              <div className="text-sm flex flex-col gap-4">
                 <div>
                   - (실험적 기능) 배경화면에 이미지를 삽입할 수 있게 되었습니다.
                   1920 x 1080 크기 이상의 이미지를 사용하기를 권장합니다.
@@ -31,8 +37,11 @@ export default function PatchNote({ open, onCancel, isLocal }: Props) {
               </div>
             </div>
             <div className="mb-4">
-              <div className="pb-4">0.3 패치내역</div>
-              <div className="text-base flex flex-col gap-4">
+              <div className="pb-4  flex justify-between  items-center">
+                <div>0.3 패치내역</div>
+                <div className="text-base text-neutral-700">2024.4.15</div>
+              </div>
+              <div className="text-sm flex flex-col gap-4">
                 <div>
                   - 일반 글자, 정확한 글자, 부정확한 글자의 색상을 사용자가
                   변경할 수 있게 되었습니다.
@@ -40,8 +49,11 @@ export default function PatchNote({ open, onCancel, isLocal }: Props) {
               </div>
             </div>
             <div>
-              <div className="pb-4">0.2 패치내역</div>
-              <div className="text-base flex flex-col gap-4">
+              <div className="pb-4  flex justify-between  items-center">
+                <div>0.2 패치내역</div>
+                <div className="text-base text-neutral-700">2024.4.14</div>
+              </div>
+              <div className="text-sm flex flex-col gap-4">
                 <div>- 스페이스바로 줄바꿈이 가능해졌습니다.</div>
                 <div>- 현재 입력중인 문자를 확인 할 수 있게 되었습니다.</div>
                 <div>- 이전에 변경한 설정사항을 저장할 수 있게 되었습니다.</div>
@@ -53,10 +65,14 @@ export default function PatchNote({ open, onCancel, isLocal }: Props) {
       ) : (
         <div className="text-lg flex-col text-neutral-800">
           <div className=" overflow-auto px-9 flex-col gap-4">
-            <div className="pb-4 font-semibold">0.41 Notes</div>
-            <div className="text-base flex flex-col gap-4">
+            <div className="pb-4 font-semibold flex justify-between items-center">
+              <div>Patch 0.41 Notes</div>
+              <div className="text-base text-neutral-700">2024.4.22</div>
+            </div>
+
+            <div className="text-sm flex flex-col gap-4">
               <div>
-                In pro mode, user-defined text color is now applied.
+                - In pro mode, user-defined text color is now applied.
                 <br />- The issue where no more characters were being input if
                 the number of characters entered exceeded the given sentence has
                 been resolved.
@@ -64,8 +80,12 @@ export default function PatchNote({ open, onCancel, isLocal }: Props) {
             </div>
 
             <div className="mb-4">
-              <div className="pb-4 font-semibold">Patch 0.4 Notes</div>
-              <div className="text-base flex flex-col gap-4">
+              <div className="pb-4  flex justify-between  items-center">
+                <div>Patch 0.4 Notes</div>
+                <div className="text-base text-neutral-700">2024.4.16</div>
+              </div>
+
+              <div className="text-sm flex flex-col gap-4">
                 <div>
                   - (Experimental Feature) You can now insert an image into the
                   background. We recommend using images of size 1920 x 1080.
@@ -73,8 +93,11 @@ export default function PatchNote({ open, onCancel, isLocal }: Props) {
               </div>
             </div>
             <div className="mb-4">
-              <div className="pb-4">Patch 0.3 Notes</div>
-              <div className="text-base flex flex-col gap-4">
+              <div className="pb-4  flex justify-between  items-center">
+                <div>Patch 0.3 Notes</div>
+                <div className="text-base text-neutral-700">2024.4.15</div>
+              </div>
+              <div className="text-sm flex flex-col gap-4">
                 <div>
                   - Users can now change the colors of normal text, accurate
                   text, and inaccurate text.
@@ -82,8 +105,11 @@ export default function PatchNote({ open, onCancel, isLocal }: Props) {
               </div>
             </div>
             <div>
-              <div className="pb-4">Patch 0.2 Notes</div>
-              <div className="text-base flex flex-col gap-4">
+              <div className="pb-4  flex justify-between  items-center">
+                <div>Patch 0.2 Notes</div>
+                <div className="text-base text-neutral-700">2024.4.14</div>
+              </div>
+              <div className="text-sm flex flex-col gap-4">
                 <div>- Line breaks can now be made using the space bar.</div>
                 <div>
                   - It is now possible to see the character currently being
