fa9e94a519af79868bf09ce6893a85b5bf5b7ec6
beomsuKim <ta09472@naver.com>
2024-04-24 16:13:05 +0900

    테스트중

---
+++ src/components/Test.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import {
+  checkCombinedFinal,
+  checkCombinedVowel,
+  toKorChars,
+} from "../util/typing";
+import { extractTextInBrackets } from "../util/color";
+
+interface Props {
+  target: string | undefined;
+  input: string;
+  color?: {
+    normal: string;
+    accuracy: string;
+    inaccuracy: string;
+  };
+}
+
+export default function Accuracy({
+  target,
+  input,
+  color = {
+    normal: "text-gray-400",
+    accuracy: "text-black",
+    inaccuracy: "text-red-500",
+  },
+}: Props) {
+  if (!target) return null;
+
+  const targetDecomposed = toKorChars(target).map((char) => ({ ...char }));
+  const inputDecomposed = toKorChars(input);
+
+  const calculateColor = (index: number) => {
+    if (index >= inputDecomposed.length) {
+      return color.normal; // 아직 입력되지 않은 인덱스
+    }
+
+    const inputChar = inputDecomposed[index];
+    const targetChar = targetDecomposed[index];
+
+    const nextTargetChar =
+      index + 1 < targetDecomposed.length ? targetDecomposed[index + 1] : null;
+
+    // 모든 조건을 종합하여 정확도를 계산
+    const isCurrentInputMatching =
+      inputChar.initial === targetChar.initial &&
+      checkCombinedVowel(inputChar.vowel, targetChar.vowel) &&
+      checkCombinedFinal(inputChar.final, targetChar.final);
+
+    // 현재 문자의 종성과 다음 문자의 초성 조합을 고려하여 정확도 계산
+    const isNextInitialMatching =
+      nextTargetChar &&
+      inputChar.final &&
+      (inputChar.final === nextTargetChar.initial ||
+        checkCombinedFinal(inputChar.final, nextTargetChar.initial));
+
+    // 조합 가능한 종성과 초성인 경우 정확도 확인
+    const combinedFinalMatching =
+      nextTargetChar &&
+      checkCombinedFinal(inputChar.final, nextTargetChar.initial);
+
+    if (index !== inputDecomposed.length - 1) {
+      const matches =
+        inputChar.initial === targetChar.initial &&
+        checkCombinedVowel(inputChar.vowel, targetChar.vowel) &&
+        checkCombinedFinal(inputChar.final, targetChar.final);
+
+      return matches ? color.accuracy : color.inaccuracy;
+    } else {
+      // 입력이 완료된 인덱스에 대해서는 기존 로직을 적용
+      const matches =
+        inputChar.initial === targetChar.initial &&
+        (checkCombinedVowel(inputChar.vowel, targetChar.vowel) ||
+          inputChar.vowel === undefined) &&
+        (checkCombinedFinal(inputChar.final, targetChar.final) ||
+          inputChar.final === undefined);
+
+      if (isNextInitialMatching) {
+        return isCurrentInputMatching ? color.accuracy : color.inaccuracy;
+      }
+
+      // if (combinedFinalMatching) {
+      //   return color.accuracy;
+      // }
+
+      return matches ? color.accuracy : color.inaccuracy;
+    }
+  };
+
+  return (
+    <div>
+      {target.split("").map((char, index) => {
+        const textColor = extractTextInBrackets(calculateColor(index));
+        if (input[index] === " ") {
+          if (char !== input[index]) {
+            return (
+              <span
+                key={index}
+                className={color.inaccuracy}
+                style={{ color: extractTextInBrackets(color.inaccuracy) }}
+              >
+                _
+              </span>
+            ); // Incorrect space input shown as "_"
+          }
+        }
+
+        if (index >= input.length) {
+          return (
+            <span
+              key={index}
+              style={{ color: extractTextInBrackets(color.normal) }}
+            >
+              {char}
+            </span>
+          );
+        }
+
+        return (
+          <span key={index} style={{ color: textColor }}>
+            {input[index]}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
